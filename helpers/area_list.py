4a4c672dbb92b5ed9d24322575147f4c4412a3a6
Paul Dumebi <paulchukwurah7@gmail.com>
2023-01-26 01:45:21 +0100

    Delete helpers directory

--- helpers/area_list.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd 
-
-#Extracting Unique areas from the covid dataframe and sorting the areas alphabet
-covid_dfl=pd. read_csv('data/specimenDate_ageDemographic-unstacked.csv', low_memory=False)
-areas = sorted(covid_dfl.areaName.unique().tolist())
-
-    
